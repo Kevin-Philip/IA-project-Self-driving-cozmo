9af768c638136886f7e1bd847ef70786813e1388
Clément BAUDET <clement.baudet@etudiant.univ-rennes1.fr>
2019-05-06 11:35:10 +0200

    hello world cozmo

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello")
+import cozmo
+
+
+def cozmo_program(robot: cozmo.robot.Robot):
+    robot.say_text("Hello guys, how are you?").wait_for_completed()
+
+
+cozmo.run_program(cozmo_program)
